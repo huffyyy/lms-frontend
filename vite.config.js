4d7cef3f39db19d929422642fdaf113f8d56995e
Husnul <husnulf936@gmail.com>
2025-11-10 06:01:21 +0700

    chnage vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@common": path.resolve(__dirname, "./src/components/common")
-    }
-  }
+  plugins: [react(), tailwindcss()]
 });
